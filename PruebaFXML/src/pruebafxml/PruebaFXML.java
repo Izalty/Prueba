4df559c825b8800e4babf552248352e1d72c4b02
TARTANGALH\iandasal <iandasal@tartanga.eus>
2025-10-07 09:18:33 +0200

    Syso PuebaFXML.java

--- PruebaFXML/src/pruebafxml/PruebaFXML.java
+++ PruebaFXML/src/pruebafxml/PruebaFXML.java
@@ -20,7 +20,7 @@ public class PruebaFXML extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        System.out.println("pruebafxml.PruebaFXML.start()");
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
